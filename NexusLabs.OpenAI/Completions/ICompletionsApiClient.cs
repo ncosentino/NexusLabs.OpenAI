62d5bd2ba240700125defdb17f692d1e24adc5ad
Nick Cosentino <n.b.cosentino@gmail.com>
2023-01-15 15:55:32 -0800

    Some Additional Completions Doc Comments

--- NexusLabs.OpenAI/Completions/ICompletionsApiClient.cs
+++ NexusLabs.OpenAI/Completions/ICompletionsApiClient.cs
@@ -1,7 +1,26 @@
 ﻿namespace NexusLabs.OpenAI.Completions
 {
+    /// <summary>
+    /// An interface that defines functionality for interacting with OpenAI's 
+    /// API for completions.
+    /// </summary>
+    /// <remarks>
+    /// See official online documentation 
+    /// <see href="https://beta.openai.com/docs/api-reference/completions">here</see>.
+    /// </remarks>
     public interface ICompletionsApiClient
     {
+        /// <summary>
+        /// Creates a completion for the provided prompt and parameters.
+        /// </summary>
+        /// <param name="parameters">
+        /// The parameters to use for the completion.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Optional. The instance of the <see cref="CancellationToken"/> to 
+        /// use.
+        /// </param>
+        /// <returns></returns>
         Task<string> CreateAsync(
             CompletionParameters parameters, 
             CancellationToken cancellationToken = default);
