2feffa63fcad12354fc327d3fb90e9e9bb2b2a89
Nick Cosentino <n.b.cosentino@gmail.com>
2023-01-15 23:32:18 -0800

    Completion Examples

--- NexusLabs.OpenAI.Examples/CompletionExamples.cs
+++ NexusLabs.OpenAI.Examples/CompletionExamples.cs
@@ -5,16 +5,89 @@ namespace NexusLabs.OpenAI.Examples
     [Example("Completions")]
     internal sealed class CompletionExamples
     {
-        [Example("CreateCompletionAdaNoPromptAsync")]
-        internal static async Task CreateCompletionAdaNoPromptAsync(IOpenAiApiClient openApiClient)
+        [Example("CreateCompletion_Simple_AdaAsync")]
+        internal static async Task CreateCompletionSimpleAdaAsync(IOpenAiApiClient openApiClient)
         {
             var parameters = new CompletionParameters("ada");
+            await ExecuteAndPrintCompletionResultsAsync(
+                openApiClient, 
+                parameters);
+        }
 
+        [Example("CreateCompletion_Simple_DavinciAsync")]
+        internal static async Task CreateCompletionSimpleDavinciAsync(IOpenAiApiClient openApiClient)
+        {
+            var parameters = new CompletionParameters("davinci");
+            await ExecuteAndPrintCompletionResultsAsync(
+                openApiClient,
+                parameters);
+        }
+
+        [Example("CreateCompletion_SinglePrompt_AdaAsync")]
+        internal static async Task CreateCompletionSinglePromptAdaAsync(IOpenAiApiClient openApiClient)
+        {
+            var parameters = new CompletionParameters(
+                Model: "ada",
+                Prompts: new string[]
+                {
+                    "Tell me a story."
+                });
+            await ExecuteAndPrintCompletionResultsAsync(
+                openApiClient,
+                parameters);
+        }
+
+        [Example("CreateCompletion_SinglePrompt_DavinciAsync")]
+        internal static async Task CreateCompletionSinglePromptDavinciAsync(IOpenAiApiClient openApiClient)
+        {
+            var parameters = new CompletionParameters(
+                Model: "davinci",
+                Prompts: new string[]
+                {
+                    "Tell me a story."
+                });
+            await ExecuteAndPrintCompletionResultsAsync(
+                openApiClient,
+                parameters);
+        }
+
+        [Example("CreateCompletion_SinglePrompt_100Max_AdaAsync")]
+        internal static async Task CreateCompletionSinglePrompt100MaxAdaAsync(IOpenAiApiClient openApiClient)
+        {
+            var parameters = new CompletionParameters(
+                Model: "ada",
+                Prompts: new string[]
+                {
+                    "Tell me a story."
+                },
+                MaxTokens: 100);
+            await ExecuteAndPrintCompletionResultsAsync(
+                openApiClient,
+                parameters);
+        }
+
+        [Example("CreateCompletion_SinglePrompt_100Max_DavinciAsync")]
+        internal static async Task CreateCompletionSinglePrompt100MaxDavinciAsync(IOpenAiApiClient openApiClient)
+        {
+            var parameters = new CompletionParameters(
+                Model: "davinci",
+                Prompts:new string[]
+                {
+                    "Tell me a story."
+                },
+                MaxTokens: 100);
+            await ExecuteAndPrintCompletionResultsAsync(
+                openApiClient,
+                parameters);
+        }
+
+        private static async Task ExecuteAndPrintCompletionResultsAsync(IOpenAiApiClient openApiClient, CompletionParameters parameters)
+        {
             Console.WriteLine("Creating completion...");
             var completion = await openApiClient
                 .Completions
                 .CreateAsync(parameters);
-            
+
             Console.WriteLine("Completion:");
             Console.WriteLine($"\tID: {completion.Id}");
             Console.WriteLine($"\tCreated UTC: {completion.CreatedUtc}");
